1de5c157bce23679bb998f63555549b511208b51
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2023-08-03 14:28:37 +0300

    main method v1.1

--- src/main/java/RestApiT.java
+++ src/main/java/RestApiT.java
@@ -2,10 +2,12 @@ import com.google.gson.Gson;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class RestApiT {
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
 
         Transcript transcript = new Transcript();
         transcript.setAudio_url("https://www.youtube.com/watch?v=0wvBu014E5o");
@@ -13,11 +15,24 @@ public class RestApiT {
         String jsonRequest = gson.toJson(transcript);
         System.out.println(jsonRequest);
 
-        HttpRequest postRequest = HttpRequest.newBuilder()
-                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript" + transcript.getId()))
                 .header("Authorization","f6ec5f2cb1f74953bc66862545ee79cc")
-                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                //.GET()
                 .build();
 
+        while(true) {
+        HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> getResponse = httpClient.send(getRequest, HttpRequest.BodyPublishers.ofString());
+            //System.out.println(getResponse.body());
+
+            transcript = gson.fromJson(getResponse.body(), Transcript.class);
+            //System.out.println(transcript.getId());
+            System.out.println(transcript.getStatus());
+
+            if ("completed".equals(transcript.getStatus() || "error".equals(transcript.getStatus())))
+                break;
+            }
+        Thread.sleep(1000);
     }
 }
